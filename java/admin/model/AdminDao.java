2b0ec52627bd9b19e132c480d6077e3fe4c6874b
tjfghkss <54305230+tjfghkss@users.noreply.github.com>
2019-12-04 19:27:24 +0900

    Add files via upload

--- java/admin/model/AdminDao.java
+++ java/admin/model/AdminDao.java
@@ -8,6 +8,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import member.model.Member;
 import seller.model.Seller;
 import utility.Paging;
 
@@ -35,8 +36,33 @@ public class AdminDao {
 		
 		return selList;
 	}
-	
-	
+
+	public int getTotalCountMem(Map<String, String> map) {
+		int totalCount = sqlSessionTemplate.selectOne(namespace + ".getTotalCountMem");
+		
+		return totalCount;
+	}
+
+	public List<Member> getMemList(Paging pageInfo, Map<String, String> map) {
+		RowBounds row = new RowBounds(pageInfo.getOffset(), pageInfo.getLimit());
+		List<Member> memList = sqlSessionTemplate.selectList(namespace + ".getMemList", map, row);
+		
+		return memList;
+	}
+
+	public void deleteMem(int m_num) {
+		sqlSessionTemplate.delete(namespace + ".deleteMem", m_num);
+	}
+
+	public Member updateView(int m_num) {
+		Member member = sqlSessionTemplate.selectOne(namespace + ".updateView", m_num);
+		
+		return member;
+	}
+
+	public void updateMem(Member member) {
+		sqlSessionTemplate.update(namespace + ".updateMem", member);
+	}
 	
 	
 	
