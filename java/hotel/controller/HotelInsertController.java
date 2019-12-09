70f44517fdd5a2d855db8873618e60e73f76bb72
ByoungHwaLee <49489205+ByoungHwaLee@users.noreply.github.com>
2019-12-09 18:33:37 +0900

    Add files via upload

--- java/hotel/controller/HotelInsertController.java
+++ java/hotel/controller/HotelInsertController.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,14 +23,17 @@ import hotel.model.Room;
 import hotel.model.RoomDao;
 import seller.model.Seller;
 
+
+
 @Controller
 public class HotelInsertController {
-	private final String command = "hotelInsert.ho";
-	private final String getPage = "hotelInsert";
+	
+	private final String command="/insert.ho";
+	private final String getPage="hotelInsertForm";
 	private final String gotoPage="redirect:/selMain.sel";
 	
 	@Autowired
-	HotelDao hoDao;
+	private HotelDao hotelDao;
 	
 	@Autowired
 	private RoomDao roomDao;
@@ -42,7 +47,7 @@ public class HotelInsertController {
 	}
 	
 	@RequestMapping(value=command,method=RequestMethod.POST)
-	public String insertPost(Room rooms, Hotel hotel,
+	public String insertPost(Room rooms,Hotel hotel,
 			MultipartHttpServletRequest mpfRequest,
 			HttpSession session) {
 		
@@ -51,12 +56,12 @@ public class HotelInsertController {
 		hotel.setSelNum(num);
 		
 		int roomcnt=0;
-		for(int i=0;i<rooms.getR_type().length;i++) {
-			String type=rooms.getR_type()[i];
-			int price=rooms.getR_price()[i];
-			int person=rooms.getR_person()[i];
-			int stock=rooms.getR_stock()[i];
-			String breakfast=rooms.getR_breakfast()[i];
+		for(int i=0;i<rooms.getType().length;i++) {
+			String type=rooms.getType()[i];
+			int price=rooms.getPrice()[i];
+			int person=rooms.getPerson()[i];
+			int stock=rooms.getStock()[i];
+			String breakfast=rooms.getBreakfast()[i];
 			String hname=hotel.getH_name();
 			
 			Room room=new Room(type,price,person,stock,breakfast,hname);
@@ -64,17 +69,18 @@ public class HotelInsertController {
 		}
 		System.out.println("객실 등록:" +roomcnt);
 		
-		String filePath=application.getRealPath("/resources/"+hotel.getH_name());
+		String filePath=application.getRealPath("/resources/Hotelimages/"+hotel.getH_name());
 		
 		List<MultipartFile> fileList=mpfRequest.getFiles("file");
 		
 		File file=new File(filePath);
+		System.out.println(filePath);
 		if(file.exists()==false) {
 			file.mkdirs();
 		}
 		String image="";
 		for(int i=0;i<fileList.size();i++) {
-			System.out.println(application.getRealPath("/resources"));
+			
 			file=new File(filePath+File.separator+fileList.get(i).getOriginalFilename());
 			try {
 				fileList.get(i).transferTo(file);
@@ -86,17 +92,9 @@ public class HotelInsertController {
 			image+=fileList.get(i).getOriginalFilename()+";";
 		}
 		hotel.setH_image(image);
-		int hotelcnt=hoDao.insertHotel(hotel);
+		int hotelcnt=hotelDao.insertHotel(hotel);
 		System.out.println("호텔등록:"+hotelcnt);
 		
 		return gotoPage;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 }
